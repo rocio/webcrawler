5a4065fd1bbec034e02caded08a7524bb08357f9
Julian Graham <jgraham@conductor.com>
2013-07-24 15:45:46 -0400

    Initial commit.

---
+++ src/main/java/org/example/webcrawler/CrawlerMain.java
@@ -0,0 +1,14 @@
+package org.example.webcrawler;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jgraham
+ * Date: 7/24/13
+ * Time: 3:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CrawlerMain {
+    public static void main(String[] argv) {
+        System.out.println("Hello, world!");
+    }
+}
